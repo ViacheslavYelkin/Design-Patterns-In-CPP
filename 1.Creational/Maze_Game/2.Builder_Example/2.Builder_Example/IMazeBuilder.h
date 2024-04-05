3fa66644168cb98f9f8c89a109030aed4e2ee63b
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-05 12:04:42 +0300

    Created IMazeBuilder.h

---
+++ 1.Creational/Maze_Game/2.Builder_Example/2.Builder_Example/IMazeBuilder.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Maze;
+class Room;
+class Door;
+
+class IMazeBuilder {
+public:
+	virtual void BuildMaze() {};
+	virtual void BuildRoom(int room_no) {};
+	virtual void BuildDoor(int room_from, int room_to) {};
+	
+	virtual Maze* GetMaze() { return nullptr; };
+
+protected:
+	IMazeBuilder();
+};
