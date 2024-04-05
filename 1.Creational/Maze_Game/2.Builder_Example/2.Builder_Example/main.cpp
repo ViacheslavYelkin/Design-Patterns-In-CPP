032fd0a65281beeede07c0493e06652d81038a99
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-05 12:54:10 +0300

    Tested functionality of StandardMazeBuilder with help of MazeGame class

--- 1.Creational/Maze_Game/2.Builder_Example/2.Builder_Example/main.cpp
+++ 1.Creational/Maze_Game/2.Builder_Example/2.Builder_Example/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
+#include "Maze.h"
+#include "MazeGame.h"
+#include "StandardMazeBuilder.h"
 
 
 int main(int argc, char** argv) {
+	Maze* maze;
+	MazeGame maze_game;
+	StandardMazeBuilder builder;
 
-
-
-
+	maze_game.CreateMaze(builder);
+	maze = builder.GetMaze();
+	
 	return EXIT_SUCCESS;
 }
