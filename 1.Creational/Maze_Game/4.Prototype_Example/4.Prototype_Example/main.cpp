2fa3f94d425eb302d63363b6ce795761a3dfdc34
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-08 13:19:17 +0300

    Tested Prototype pattern in main.cpp

--- 1.Creational/Maze_Game/4.Prototype_Example/4.Prototype_Example/main.cpp
+++ 1.Creational/Maze_Game/4.Prototype_Example/4.Prototype_Example/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 
-
+#include "MazeGame.h"
+#include "MazePrototypeFactory.h"
+#include "Maze.h"
+#include "Room.h"
+#include "Wall.h"
+#include "Door.h"
 
 int main(int argc, char** argv) {
 
+	MazeGame game;
+	MazePrototypeFactory prototype{
+		new Maze, new Wall, new Room, new Door
+	};
 
+	Maze* maze = game.CreateMaze(prototype);
 
 	return EXIT_SUCCESS;
 }
