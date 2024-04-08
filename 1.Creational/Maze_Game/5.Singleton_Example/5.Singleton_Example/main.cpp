37e9102eba993844f0ef96412a3e6c1554ce7463
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-08 15:00:13 +0300

    Tested Singleton pattern in main.cpp

--- 1.Creational/Maze_Game/5.Singleton_Example/5.Singleton_Example/main.cpp
+++ 1.Creational/Maze_Game/5.Singleton_Example/5.Singleton_Example/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+#include "MazeFactory.h"
 
 
 int main(int argc, char** argv) {
 
+	MazeFactory* factory1 = MazeFactory::Instance();
+	MazeFactory* factory2 = MazeFactory::Instance();
 
-
+	std::cout << factory1 << '\n'; 
+	std::cout << factory2 << '\n'; //should print same memory address
+	
 	return EXIT_SUCCESS;
 }
