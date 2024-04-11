f5e26b4eafcd39e9c2c9f96a8d5d78394020be87
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-11 13:53:30 +0300

    Added const modifier to getSide function

--- 2.Structural/1.Adapter_Example/1.Adapter_Example/Square.h
+++ 2.Structural/1.Adapter_Example/1.Adapter_Example/Square.h
@@ -5,7 +5,7 @@ class Square {
 public:
 	explicit Square(const int side) : side_{side} {}
 
-	int getSide() { return side_; }
+	int getSide() const { return side_; }
 	void setSide(int value) { side_ = value; }
 
 private:
