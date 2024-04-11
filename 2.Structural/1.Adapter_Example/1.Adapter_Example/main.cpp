773cc5c55207ae0b0176504ab7734606ad37f22d
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-11 14:00:15 +0300

    Tested implemented Adapter pattern in main.cpp

--- 2.Structural/1.Adapter_Example/1.Adapter_Example/main.cpp
+++ 2.Structural/1.Adapter_Example/1.Adapter_Example/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 
-
+#include "Square.h"
+#include "SquareToRectangleAdapter.h"
 
 
 int main(int argc, char** argv) {
 
+	Square* sq = new Square(11);
 
+	SquareToRectangleAdapter sra(*sq);
+
+	std::cout << sra.area() << std::endl;
 
 	return EXIT_SUCCESS;
 }
