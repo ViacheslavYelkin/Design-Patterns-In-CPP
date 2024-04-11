616bb7aaeb2e69201eb1ae2e693b83e952fac100
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-11 17:21:12 +0300

    Tested implementation of BridgePattern in main.cpp

--- 2.Structural/2.Builder_Example/2.Builder_Example/main.cpp
+++ 2.Structural/2.Builder_Example/2.Builder_Example/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <memory>
 
-
+#include "Renderer.h"
+#include "VectorRenderer.h"
+#include "RasterRenderer.h"
+#include "Shape.h"
+#include "Circle.h"
+#include "Square.h"
 
 int main(int argc, char** argv) {
 
+	std::unique_ptr<IShape> shape1 = std::make_unique<Circle>(new VectorRenderer());
+	std::unique_ptr<IShape> shape2 = std::make_unique<Square>(new RasterRenderer());
+	shape1->draw();
+	shape2->draw();
 
 
 	return EXIT_SUCCESS;
