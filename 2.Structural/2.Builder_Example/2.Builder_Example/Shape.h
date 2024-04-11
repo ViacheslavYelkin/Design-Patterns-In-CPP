750bea00d8d5107d08bf46e3cd78e648d8ec7fcb
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-11 17:13:07 +0300

    Moved draw function to public section

--- 2.Structural/2.Builder_Example/2.Builder_Example/Shape.h
+++ 2.Structural/2.Builder_Example/2.Builder_Example/Shape.h
@@ -9,8 +9,9 @@ protected:
 	IShape(IRenderer* rn, const std::string& name)
 		: rn_{ rn }, name_{ name }
 	{}
-	virtual void draw() = 0;
 
 	IRenderer* rn_{};
 	std::string name_{};
+public:
+	virtual void draw() = 0;
 };
