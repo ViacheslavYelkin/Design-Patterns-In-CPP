51632ce5413f27a050ac5dd50af657a6cc54b85a
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-15 13:54:35 +0300

    Tested Decorator pattern in main.cpp

--- 2.Structural/4.Decorator_Example/4.Decorator_Example/main.cpp
+++ 2.Structural/4.Decorator_Example/4.Decorator_Example/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <string>
 
+#include "Circle.h"
+#include "Square.h"
+#include "ColoredShape.h"
+#include "BorderedShape.h"
 
 int main(int argc, char** argv) {
 
+	Circle circle{4};
+	Square square{};
 
+	ColoredShape col_shape{circle, "black"};
 
+	BorderedShape bor_shape1{ col_shape, 2.5, "red" };
+	BorderedShape bor_shape2{ square, 15, "green" };
+
+	circle.draw();
+	std::cout << '\n';
+
+	col_shape.draw();
+	std::cout << '\n';
+	
+	bor_shape1.draw();
+	std::cout << '\n';
+
+	bor_shape2.draw();
+	std::cout << '\n';
 
 	return EXIT_SUCCESS;
 }
