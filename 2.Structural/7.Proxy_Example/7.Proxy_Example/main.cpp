495d063984d7d88303c70ccd1279258b4211cfd1
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-16 16:15:43 +0300

    Tested Proxy pattern in main.cpp

--- 2.Structural/7.Proxy_Example/7.Proxy_Example/main.cpp
+++ 2.Structural/7.Proxy_Example/7.Proxy_Example/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
-
+#include "Person.h"
+#include "ResponsiblePerson.h"
 
 int main(int argc, char** argv) {
 
+	ResponsiblePerson child(Person(13));
+	std::cout << child.drink() << '\n' << child.drive() << '\n';
 
+	std::cout << "-------------------------------------------\n";
+
+	child.setAge(16);
+	std::cout << child.drink() << '\n' << child.drive() << '\n';
+	
+	std::cout << "-------------------------------------------\n";
+
+	ResponsiblePerson adult(Person(20));
+	std::cout << adult.drink() << '\n' << adult.drive() << '\n';
+	std::cout << adult.drinkAndDrive() << '\n';
 
 	return EXIT_SUCCESS;
 }
