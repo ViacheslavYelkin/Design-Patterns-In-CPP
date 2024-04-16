a07b62ea02d31b8c7c4c77199612e615ce7c8d00
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-16 14:13:11 +0300

    Tested Flyweight pattern in main.cpp

--- 2.Structural/6.Flyweight_Example/6.Flyweight_Example/main.cpp
+++ 2.Structural/6.Flyweight_Example/6.Flyweight_Example/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
-
+#include "User.h"
 
 int main(int argc, char** argv) {
 
+	User user1{ "Sam", "Smith" };
+	User user2{ "Bob", "Smith" };
 
+	std::cout << user1 << user2;
 
 	return EXIT_SUCCESS;
 }
