2940663694065195394b1404b5448c53c047afb3
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-17 14:34:49 +0300

    Tested 'Chain of Responsibility' pattern

--- 3.Behavioral/1.Chain_of_Responsibility_Example/1.Chain_of_Responsibility_Example/main.cpp
+++ 3.Behavioral/1.Chain_of_Responsibility_Example/1.Chain_of_Responsibility_Example/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 
-
+#include "Creature.h"
+#include "CreatureModifier.h"
+#include "DoubleDefenseModifier.h"
+#include "DoubleAttackModifier.h"
 
 int main(int argc, char** argv) {
 
+	Creature baby_orc{ "Baby Orc", 1, 1 };
 
+	CreatureModifier root{baby_orc};
+
+	DoubleAttackModifier atck_mod{ baby_orc };
+	DoubleDefenseModifier def_mod{ baby_orc };
+
+	root.add(&def_mod);
+	root.add(&atck_mod);
+
+	root.handle();
+
+	std::cout << baby_orc;
 
 	return EXIT_SUCCESS;
 }
