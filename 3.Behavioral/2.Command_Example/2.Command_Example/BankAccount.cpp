2ef2146c54c317ac96f6761818ba647fd1bccf67
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-18 14:23:46 +0300

    Created BankAccount.h/.cpp & added BankAccount class

---
+++ 3.Behavioral/2.Command_Example/2.Command_Example/BankAccount.cpp
@@ -0,0 +1,31 @@
+#include "BankAccount.h"
+
+BankAccount::BankAccount() :
+	success_{false}
+{
+}
+
+bool BankAccount::deposite(int amount)
+{
+	balance_ += amount;
+	std::cout << *this;
+	return success_ = true;
+}
+
+bool BankAccount::withdraw(int amount)
+{
+	if ((balance_ - amount) >= 0) {
+		balance_ -= amount;
+		std::cout << *this;
+		return success_ = true;
+	}
+
+	std::cout << "Operation failed";
+	return success_ = false;
+}
+
+std::ostream& operator<<(std::ostream& os, const BankAccount& obj)
+{
+	os << "Bank account #" << &obj << " current balance: " << obj.balance_;
+	return os;
+}
---
+++ 3.Behavioral/2.Command_Example/2.Command_Example/BankAccount.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+
+class BankAccount {
+	friend std::ostream& operator<<(std::ostream& os, const BankAccount& obj);
+
+public:
+
+	BankAccount();
+
+	bool deposite(int amount);
+	bool withdraw(int amount);
+
+private:
+	int balance_{};
+	bool success_{};
+};
