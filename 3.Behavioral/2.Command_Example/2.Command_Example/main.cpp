467a5b630f605c46ad0f0609cad464c304266d0f
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-18 15:23:09 +0300

    Tested Command pattern in main.cpp

--- 3.Behavioral/2.Command_Example/2.Command_Example/main.cpp
+++ 3.Behavioral/2.Command_Example/2.Command_Example/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 
+#include "BankAccount.h"
+#include "BankAccountCommand.h"
+#include "CompositeBankAccountCommand.h"
 
 int main(int argc, char** argv) {
 
+	BankAccount acc;
+	BankAccountCommand cmd{acc, BankAccountAction::withdraw, 200};
 
+	CompositeBankAccountCommand cbac{ 
+		{
+			BankAccountCommand{acc, BankAccountAction::deposite, 100},
+			BankAccountCommand{acc, BankAccountAction::deposite, 100},
+			BankAccountCommand{acc, BankAccountAction::withdraw, 50},
+		} 
+	};
+
+	cbac.call();
+
+	std::cout << "------------------------------\n";
+
+	cbac.undo();
 
 	return EXIT_SUCCESS;
 }
