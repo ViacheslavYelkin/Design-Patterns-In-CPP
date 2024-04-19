e82945f89e2ae421c80bd58e6f99e7b4bfe92074
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-19 14:20:13 +0300

    Created Element.h & added Element interface

---
+++ 3.Behavioral/3.Interpreter_Example/3.Interpreter_Example/Element.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+
+class Element {
+public:
+	virtual int eval() const = 0;
+};
--- 3.Behavioral/3.Interpreter_Example/3.Interpreter_Example/main.cpp
+++ 3.Behavioral/3.Interpreter_Example/3.Interpreter_Example/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-
+#include "Token.h"
+#include "ETokenType.h"
 
 int main(int argc, char** argv) {
 
+	std::string input{"1+3+4-2"};
 
+	std::vector<Token> tokens = Token::lex(input);
 
+	for (const Token& t : tokens) {
+		std::cout << t << " ";
+	}
 
 	return EXIT_SUCCESS;
 }
