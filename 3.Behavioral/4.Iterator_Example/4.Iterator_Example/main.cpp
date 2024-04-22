119c67d5bc6f2207bfe34390e38ddc3082c269c1
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-22 15:45:28 +0300

    Tested Iterator pattern in main.cpp

--- 3.Behavioral/4.Iterator_Example/4.Iterator_Example/main.cpp
+++ 3.Behavioral/4.Iterator_Example/4.Iterator_Example/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
 
+#include "List.h"
 
 int main(int argc, char** argv) {
 
+	Node<int>* root = new Node<int>(1);
 
+	List<int> ls(root);
+
+	ls.add(new Node<int>(2));
+	ls.add(new Node<int>(3));
+	ls.add(new Node<int>(4));
+
+
+	List<int>::iterator it = ls.begin();
+
+	for (; it != ls.end(); ++it) {
+		std::cout << it->value_ << " ";
+	}
 
 	return EXIT_SUCCESS;
 }
