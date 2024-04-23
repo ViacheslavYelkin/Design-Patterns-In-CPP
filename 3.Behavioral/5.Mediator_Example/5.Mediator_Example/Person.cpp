7291f32d2601166525f3ce44690694bff6ee530c
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-23 14:07:28 +0300

    Added logic to methods in Person.cpp

--- 3.Behavioral/5.Mediator_Example/5.Mediator_Example/Person.cpp
+++ 3.Behavioral/5.Mediator_Example/5.Mediator_Example/Person.cpp
@@ -1,4 +1,6 @@
 #include "Person.h"
+#include "ChatRoom.h"
+#include <iostream>
 
 Person::Person(const std::string& name) 
 	: name_{name}
@@ -7,12 +9,17 @@ Person::Person(const std::string& name)
 
 void Person::say(const std::string& message) const
 {
+	room_->broadcast(name_, message);
 }
 
 void Person::pm(const std::string& who, const std::string& message) const
 {
+	room_->message(name_, who, message);
 }
 
 void Person::receive(const std::string& origin, const std::string& message)
 {
+	std::string ms{ origin + ": \"" + message + '\"' };
+	std::cout << "[" << name_ << "'s session]: " << ms << '\n';
+	chat_log_.push_back(ms);
 }
