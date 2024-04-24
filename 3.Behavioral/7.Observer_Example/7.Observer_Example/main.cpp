734e9ee8375eb238a389190c8a845b68b7941d09
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-24 14:40:48 +0300

    Tested Observer pattern in main.cpp

--- 3.Behavioral/7.Observer_Example/7.Observer_Example/main.cpp
+++ 3.Behavioral/7.Observer_Example/7.Observer_Example/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
-
-
+#include "BookStore.h"
+#include "Customer.h"
 
 int main() {
+
+	BookStore store{{"1984", "Design Patterns", "C++ Handbook", "Mystery stories of Landfield"}};
+
+	Customer c1{"1984", store};
+	Customer c2{ "How its work?", store};
+	Customer c3{ "Shrek", store };
+
+	c1.buyBook();
+	c2.buyBook();
+	c3.buyBook();
+
+	store.addBook("Shrek");
+	store.addBook("How its work?");
 
 
 
