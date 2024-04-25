b03c490ce39f2ba652c9292edcd20f7aab0e5d7a
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-25 13:57:25 +0300

    Test Strategy pattern in main.cpp

--- 3.Behavioral/9.Strategy_Example/9.Strategy_Example/main.cpp
+++ 3.Behavioral/9.Strategy_Example/9.Strategy_Example/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-
+#include "OutputFormat.h"
+#include "TextProcessor.h"
 
 int main(int argc, char** argv) {
 
+	std::vector <std::string> items{"item1", "item2", "item3"};
 
+	TextProcessor tp{};
+	tp.setOutputFormat(OutputFormat::markdown);
+	tp.appendList(items);
+
+	std::cout << tp.str() << '\n';
+	tp.clear();
+
+	tp.setOutputFormat(OutputFormat::html);
+	tp.appendList(items);
+	
+	std::cout << tp.str() << '\n';
 
 	return EXIT_SUCCESS;
 }
